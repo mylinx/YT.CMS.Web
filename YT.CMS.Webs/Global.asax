596b7cc38afb71a013c4690f477f13cd6d0fa99b
mylinx <380440561@qq.com>
2019-06-17 10:53:12 +0800

    1.增加一个Webs版本

---
+++ YT.CMS.Webs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YT.CMS.Webs.MvcApplication" Language="C#" %>
---
+++ YT.CMS.Webs/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+namespace YT.CMS.Webs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
